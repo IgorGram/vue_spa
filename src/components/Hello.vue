68248a5eb94e30c5687ed465dc63ff004163b53b
Igor <grammatchikov@inbox.ru>
2018-07-24 19:30:47 +0300

    done

---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+	export default {
+		name: "Hello"
+	}
+</script>
+
+<style lang="scss">
+    .greeting {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        flex-direction: column;
+    }
+</style>
